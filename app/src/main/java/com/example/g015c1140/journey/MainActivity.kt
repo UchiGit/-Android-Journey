2fa8700a3ff01cc22d2e2293ae6c827ec12f4083
G015C1140\G015C1140 <horizon12.kuro@gmail.com>
2018-07-11 12:46:51 +0900

    toolbar,bottombar 被ってる

--- app/src/main/java/com/example/g015c1140/journey/MainActivity.kt
+++ app/src/main/java/com/example/g015c1140/journey/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.g015c1140.journey
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
+import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
@@ -13,9 +14,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //タイトル名セット
         title = "プラン投稿"
-      //  var actionBar = actionBar
-      //  actionBar.setDisplayHomeAsUpEnabled(true)
+
+        //ツールバーセット
+        var toolbar = toolbar
+        setSupportActionBar(toolbar)
+        //戻るボタンセット
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        supportActionBar!!.setHomeButtonEnabled(true)
 
         var bottomavigation: BottomNavigationView = findViewById(R.id.navigation)
         // BottomNavigationViewHelperでアイテムのサイズ、アニメーションを調整
@@ -23,16 +30,25 @@ class MainActivity : AppCompatActivity() {
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
     }
 
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.toolbar, menu)
+        return true
+    }
+
+
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             android.R.id.home -> {
-                Toast.makeText(this, "もどーるぼたんたっぷど",Toast.LENGTH_SHORT)
+                //戻るボタンタップ時
+                Toast.makeText(this, "もどーるぼたんたっぷど",Toast.LENGTH_SHORT).show()
                 return true
             }
         }
         return super.onOptionsItemSelected(item)
     }
 
+    //ボトムバータップ時
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_home -> {
